4241a5d5551f851b5b81d416b62e44eaa7f7a07b
srbberlin <info@hans-schrieber.de>
2018-01-08 14:43:49 +0100

    Added axis for time and value

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-import * as d3 from "d3"
-
 let url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 let height = 400
 let colWidth = 3
@@ -19,30 +17,78 @@ let svg = d3.select('svg')
 let main = d3.select('.main')
 let chart = d3.select('.chart')
   .attr('transform', 'translate(15, 15)')
-  .attr('height', height)    
+  .attr('height', height)
+let box = d3.select('.box')
+let foObj = d3.select('foreignObject')
+let head = d3.select('.text thead')
+let body = d3.select('.text tbody')
+let footer = d3.select('.text tfoot')
 
 d3.json(url, (error, res) => {
   if (! error) {
     let dateFrom = new Date(res.from_date)
     let dateTo = new Date(res.to_date)
     let source = res.source_name
-    let name = res.name
-    let des = res.description
+    let name = res.name.split(',')[0] + ' of the USA'
+    let des = res.description.split('\n')
     let value = res.data
     let width = colWidth * value.length
 
-    let scale = d3.scaleLinear()
+    let barScale = d3.scaleLinear()
       .domain([0, mx(value)])
       .range([0, height])
 
+    let valueScale = d3.scaleLinear()
+      .domain([0, mx(value)])
+      .range([height, 0])
+
+    let timeScale = d3.scaleTime()
+      .domain([dateFrom, dateTo])
+      .range([0, width])  
+
+    let timeAxis = d3.axisBottom(timeScale)
+
+    let valueAxis = d3.axisRight(valueScale)
+
+    des.push('Source: ' + source)
+
     svg
       .attr('height', height + 90)
-      .attr('width', width + 30)
+      .attr('width', width + 100)
 
     main
       .attr('height', height + 90)
-      .attr('width', width + 30)
+      .attr('width', width + 100)
   
+    box
+      .attr('transform', 'translate(25, 25)')
+      .attr('height', 250)
+      .attr('width', 350)
+
+    foObj
+      .attr('transform', 'translate(25, 25)')
+      .attr('height', 250)
+      .attr('width', 350)
+
+    head
+      .html('<td colspan="2">' + name + '</td>')
+
+    body
+      .selectAll('tbody')
+      .data(des)
+      .enter()
+      .append('tr')
+      .html((d) => {
+        let v = d.split(': ')
+        if (v[1]) {
+          v[1] = v[1].replace(/\((http.*?)\)/i, '<a href=$1 target=_blank>$1</a>')
+        }
+        return '<td>' + v[0] + '</td><td>' + v[1] + '</td>'
+      })
+
+    footer
+      .html('<td colspan="2">' + 'TEST' + '</td>')
+
     chart.attr('width', width)
       .selectAll('g')
       .data(value)
@@ -50,9 +96,21 @@ d3.json(url, (error, res) => {
       .append('g')
       .append('rect')
       .attr('width', colWidth - 1)
-      .attr('height', (d) => { return scale(d[1]) })
+      .attr('height', (d) => { return barScale(d[1]) })
       .attr('transform', (d, i) => {
-        return 'translate(' + i * colWidth + ',' + (height - scale(d[1])) + ')'
+        return 'translate(' + i * colWidth + ',' + (height - barScale(d[1])) + ')'
       })
+
+    chart
+      .append('g')
+      .attr('transform', 'translate(0, ' + (height + 10) + ')')
+      .call(timeAxis)
+
+    chart
+      .append('g')
+      .attr('transform', 'translate(' + (width + 10) + ', 0)')
+      .call(valueAxis)
+      .selectAll('.tick text')
+      .style('text-anchor', 'start')
   }
 })
